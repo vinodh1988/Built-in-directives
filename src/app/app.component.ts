ef6fd9c0e88ff3d0363dc3275363970abda4edc2
vinodh1988 <k.c.vinodh@hotmail.com>
2017-11-20 21:11:54 +0530

    First Commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,20 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'app';
+  title:string = 'My ANG2 APP';
+  color:string="lime";
+  theme:string="light-version";
+  concepts:string[]=['Component',
+'App Structure',
+'Directives',
+'Attribute directives',
+'Structural Directives'];
+
+  changeColor(){
+    
+    let colors:string[]=
+    ['yellow','tomato','lime','lightgreen',
+    'orange'];
+    this.color=colors[Math.floor(Math.random()*5)];
+  }
 }
